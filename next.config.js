e719e4a719a2952f3c034cc14e741e264655c0ba
Rohit Bhatt <rbhatt199924@gmail.com>
2023-09-24 22:13:42 +0530

    update rules for eslint

--- next.config.js
+++ next.config.js
@@ -1,19 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
-          {
-            source: '/categories',
-            destination: '/blogs',
-            permanent: true,
-          },
-          {
-            source: '/archive',
-            destination: '/blogs',
-            permanent: true,
-          }
-        ]
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/categories',
+        destination: '/blogs',
+        permanent: true,
       },
+      {
+        source: '/archive',
+        destination: '/blogs',
+        permanent: true,
+      }
+    ]
+  },
 }
 
 module.exports = nextConfig
