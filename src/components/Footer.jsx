5944aa0b3db7c001d6a1bf09f14b3650c067574e
= <=>
2024-10-04 14:01:50 +0530

    fix typo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ import { FiMail } from 'react-icons/fi'
 export default function Footer() {
     return (
         <div className="flex flex-col items-center justify-center gap-4 bg-neutral-900 py-5">
-            <p className={`text-sm md:text-lg font-normal text-center md:text-left text-white`}>© 2023 Rohit Bhatt. All rights reserved.</p>
+            <p className={`text-sm md:text-lg font-normal text-center md:text-left text-white`}>© {new Date().getFullYear()} Rohit Bhatt. All rights reserved.</p>
             <div className="flex flex-row gap-4 justify-center md:justify-start">
                 <Link href='mailto:rbhatt199924@gmail.com' target='_blank' rel="noreferrer"><FiMail className="hover:scale-105 cursor-pointer w-8 h-8 text-white hover:text-blue-500" /></Link>
                 <Link href='https://github.com/rbhatt1999' target='_blank' rel="noreferrer"><AiFillGithub className="hover:scale-105 cursor-pointer w-8 h-8 text-white hover:text-blue-500" /></Link>
